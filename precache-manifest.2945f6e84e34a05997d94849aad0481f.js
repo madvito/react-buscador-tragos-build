e61d4da691f4438650ba94649f00bc8d83fa8d58
Victor <vitomad89@gmail.com>
2020-05-04 00:18:50 -0400

    first commit

---
+++ precache-manifest.2945f6e84e34a05997d94849aad0481f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb94e899c35a8ba5393f11fa70625ccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdc424818a9307c52e6c",
+    "url": "/static/css/main.3ba26e54.chunk.css"
+  },
+  {
+    "revision": "1fb61487712f2228d514",
+    "url": "/static/js/2.a21f9ed8.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/static/js/2.a21f9ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc424818a9307c52e6c",
+    "url": "/static/js/main.2844a364.chunk.js"
+  },
+  {
+    "revision": "b8b4fb0c83d2314b6f59",
+    "url": "/static/js/runtime-main.e8a9c0c0.js"
+  }
+]);
